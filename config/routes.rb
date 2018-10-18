00c67385c4e729b1cd10df2c8ab3ebf92fba1db2
JuanCMurillo <43974413+JuanCMurillo@users.noreply.github.com>
2018-10-18 17:43:42 -0500

    proyectogit
    Proyecto_Fedesoft

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root to:"pages#index"
 
   get 'about' =>'pages#about'
+
+  get 'register' => 'pages#register'
+
+  get 'sign_in' => 'pages#sign_in'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
