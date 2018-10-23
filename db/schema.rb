9ab34b90ec2420e00a179b47317f6d625be29f78
JuanCMurillo <43974413+JuanCMurillo@users.noreply.github.com>
2018-10-22 20:22:43 -0500

    commit pagina inicial

--- db/schema.rb
+++ db/schema.rb
@@ -9,16 +9,3 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20181016234824) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-    t.string "nombre"
-    t.text "descripcion"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-end
